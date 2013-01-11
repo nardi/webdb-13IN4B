db8260fa0da5be0e6af33104f595c64feae03512
Nardi Lam <me@nardilam.nl>
2013-01-11 11:20:38 +0100

    db.php is nu main.php met standaard exception-handler

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php    
+    function show_error_page($exception)
+    {
+        echo "Er ging iets mis bij het laden van deze pagina:</br></br>" . $exception->getMessage();
+    }
+    
+    set_exception_handler('show_error_page');
+    
+    function connect_to_db()
+    {
+        $mysqli = new mysqli("localhost", "webdb13IN4B", "trestunu", "webdb13IN4B");
+        if ($mysqli->connect_errno)
+        {
+            throw new Exception($mysqli->connect_error);
+        }
+        return $mysqli;
+    }
+?>
