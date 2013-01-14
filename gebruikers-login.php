d2533d927134f458a3b228c5296b8d7d234587fb
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-14 14:42:02 +0100

    Eerste test-versie inloggen - gebruikt oude tabel.

---
+++ gebruikers-login.php
@@ -0,0 +1,25 @@
+<?php
+    require 'main.php';
+
+    $db = connect_to_db();
+	$emailadres = $_POST['e-mailadres'];
+    $wachtwoord = $_POST['wachtwoord'];
+	
+	$sql = “SELECT wachtwoord FROM `database_registratie` WHERE  email = $emailadres”;
+	$wwdb = $db->query($sql);
+	
+	$wwhash = hash('sha256', $wachtwoord);
+	$salthash = str_split($wwdb, 64);
+	$salt = $salthash[0];
+	$saltedwwhash = hash('sha256', $salt . $wwhash);
+	
+	if($saltedwwhash == $salthash[1]) {
+		echo "Succes";
+	}
+	else {
+		echo "Fail";
+	}
+?>
+	
+	
+	
