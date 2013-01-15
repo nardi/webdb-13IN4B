55a509180a3a226d23cb5d21fa792f2bcd45645d
Nardi Lam <me@nardilam.nl>
2013-01-15 16:13:26 +0100

    PHP-wrapper voor postcode-api toegevoegd

--- adres.php
+++ adres.php
@@ -2,5 +2,5 @@
     $postcode = $_GET['postcode'];
     $nr = $_GET['nr'];
     
-    echo file_get_contents('https://d9liJoGnbWooCGhi1K5xqiqgq6tX6A65Iv3gXMAWtxh:ovoNbJYrM4aPZetTdvdkzj9RVTovfqaFtZhkjCIBxJE@api.postcode.nl/rest/addresses/' . postcode + '/' . nr);
+    echo file_get_contents('https://d9liJoGnbWooCGhi1K5xqiqgq6tX6A65Iv3gXMAWtxh:ovoNbJYrM4aPZetTdvdkzj9RVTovfqaFtZhkjCIBxJE@api.postcode.nl/rest/addresses/' . $postcode + '/' . $nr);
 ?>
