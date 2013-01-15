38ad64f9f9457a2c96d67b152eb49012df02e1bc
Nardi Lam <me@nardilam.nl>
2013-01-15 13:44:58 +0100

    Testcontrole PayPalbetalingen

--- ipn.php
+++ ipn.php
@@ -1,7 +1,26 @@
 <?php
-    function post($url, $data)
+    function https_post($url, $data)
     {
-        $url = parse_url($url);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+        
+        if(!($result = curl_exec($ch)))
+        {
+            error_log("Got " . curl_error($ch) . " when processing IPN data");
+            curl_close($ch);
+            exit();
+        }
+        
+        curl_close($ch);
+        
+        /* $url = parse_url($url);
         $host = $url['host'];
         $path = $url['path'];
         
@@ -28,11 +47,11 @@
         fclose($fp);
         $result = explode("\r\n\r\n", $result, 2);
      
-        return isset($result[1]) ? $result[1] : '';
-    }
+        return isset($result[1]) ? $result[1] : ''; */
+    }   
     
     $post_data = file_get_contents('php://input');
     $verify_data = "cmd=_notify-validate&" . $post_data;
-    $result = post("https://www.sandbox.paypal.com/nl/cgi-bin/webscr", $verify_data);
-    error_log($verify_data . "\n\n" . "'$result'", 1, "mij@nardilam.nl");
+    $result = https_post("https://www.sandbox.paypal.com/nl/cgi-bin/webscr", $verify_data);
+    error_log($verify_data . "\n\n" . "'$result'");
 ?>
