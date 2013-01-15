513669496e2820984b819f1fd825e35f4ebd370c
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-15 14:13:16 +0100

    naam toegevoegd bij login

--- index.php
+++ index.php
@@ -28,16 +28,17 @@
             <a href="http://sisv2.tk/index.php?pag=account-overzicht.html">Mijn account</a><br />
             <a href="http://sisv2.tk/index.php?pag=cart.html">Winkelwagen (3)</a>
             </div>
+			
 			<?php 
 				session_start();
 				if (isset($_SESSION['logged-in'])) {
-					echo "Ingelogd";
-					
+					echo $_SESSION['gebruiker-naam'];
 				}
 				else {
 					echo "Niet ingelogd";
 				}
 			?>
+			
         </div>
     </div>
 
