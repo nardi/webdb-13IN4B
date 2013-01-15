b5c25c1374715a87d61ac2efe5d7af014499bc1f
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-15 15:18:40 +0100

    Product-toevoegen.html -> product-toevoegen.php. product-toevoegen.php -> product-toevoegen-db.php. Error toegevoegd voor niet-admins

--- product-toevoegen.php
+++ product-toevoegen.php
@@ -1,26 +1,76 @@
+<link rel="stylesheet" type="text/css" href="inloggen-wachtwoord-registratie.css">
+<link rel="stylesheet" type="text/css" href="centering.css">
+ 
 <?php
-    require 'main.php';
+	session_start();
+	if ((!isset($_SESSION['logged-in'])) || ($_SESSION['gebruiker-status'] < 3)) {
+		?>
+					_.._
+				   (_.-.\
+			   .-,       `
+		  .--./ /     _.-""-.
+		   '-. (__..-"       \
+			  \          a    |
+			   ',.__.   ,__.-'/
+				 '--/_.'----'`
+		Whale whale whale, what have we here?
+		
+		<?php
+	}
+	else {
+		?>
+		<div class="centered-container">
+			<div class="product-toevoegen">
+				<form action="product-toevoegen-db.php" method="post">
+				  <div align="right"> 
+				  <h1><center><b>Product toevoegen</b></center></h1>
+					  <hr width="100%">
+					  <center><b>Productspecificaties</b></center>
+					  <br>
+					  Titel: <input type="text" name="titel"><br>
+					  Beschrijving: <input type="text" name="beschrijving"><br>
+					  Prijs: <input type="text" name="prijs"><br>
+					  Release date: <input type="text" name="release_date"><br>
+					  Voorraad: <input type="text" name="voorraad"><br>
+					  Platform:
+					  <div class="platform">
+		  
+						  <select name="platform">
+						  <option value="ps3">PS3</option>
+						  <option value="ps2">PS2</option>
+						  <option value="psp">PSP</option>
+						  <option value="psvita">PlayStation Vita</option>
+						  <option value="xbox">XBOX 360</option>
+						  <option value="pc">PC</option>
+						  <option value="nds">Nintendo DS</option>
+						  <option value="n3ds">Nintendo 3DS</option>  
+						  <option value="wiiu">Wii U</option>
+						  <option value="wii">Wii</option>
+						  <option value="pc">PC</option>
+						  </select>
 
-    $db = connect_to_db();
+					  </div>
+					  <br/>              
+					  Genre:
+					  <div class="genre">
 
-    $titel = safe_POST('titel', $db);
-    $beschrijving = safe_POST('beschrijving', $db);
-    $prijs = safe_POST('prijs', $db);
-    $release_date = safe_POST('release_date' , $db);
-    $voorraad = safe_POST('voorraad', $db);
-    $platform = safe_POST('platform' , $db);
-    $genre = safe_POST('genre', $db);
-    
-    
-    $sqli_producten = $db->prepare("INSERT INTO Producten (titel, beschrijving, prijs, release_date, voorraad, platform, genre)
-    VALUES (?,?,?,?,?,?,?)");
-    
-    $sqli_producten->bind_param('sssssss',$titel, $beschrijving, $prijs, $release_date, $voorraad, $platform, $genre);
+						  <select name="genre">
+						  <option value="actie">Actie</option>
+						  <option value="race">Race</option>
+						  <option value="sport">Sport</option>
+						  <option value="virtueelleven">Virtueel leven</option>
+						  <option value="avontuur">Avontuur</option>
+						  <option value="partyenmuziek">Party & Muziek</option>
+						  <option value="strat">Strategie</option>
+						  </select>
 
-    if(!$sqli_producten->execute())
-        throw new Exception($sqli_producten->error);
-
-    $db->close();
-    
-    redirect_to("index.php?pag=product-toevoegen-succesvol.html");
-?>
+					  </div>
+					  <br/>
+					  <div align="right">
+					  
+					  </div>
+					  <input type="submit" value="Voeg product toe">
+				  </div>
+				</form>
+			</div>
+		</div>
