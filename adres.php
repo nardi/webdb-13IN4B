7a0d6ebd67521e57f56ab26e73738b0faa1de1b4
Nardi Lam <me@nardilam.nl>
2013-01-16 14:47:48 +0100

    Dynamische categoriepagina toegevoegd

--- adres.php
+++ adres.php
@@ -1,12 +1,5 @@
 <?php
-    function get_address($postcode, $nummer, $toevoeging = '')
-    {
-        $ch = curl_init("https://api.postcode.nl/rest/addresses/$postcode/$nummer/$toevoeging");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERPWD, "d9liJoGnbWooCGhi1K5xqiqgq6tX6A65Iv3gXMAWtxh:ovoNbJYrM4aPZetTdvdkzj9RVTovfqaFtZhkjCIBxJE");
-        
-        return curl_exec($ch);
-    }
+    require 'main.php';
     
     echo get_address($_GET['postcode'], $_GET['nummer'], isset($_GET['toevoeging']) ? $_GET['toevoeging'] : '');
 ?>
--- main.php
+++ main.php
@@ -27,4 +27,13 @@
     {
         header("location:$url");
     }
+    
+    function get_address($postcode, $nummer, $toevoeging = '')
+    {
+        $ch = curl_init("https://api.postcode.nl/rest/addresses/$postcode/$nummer/$toevoeging");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERPWD, "d9liJoGnbWooCGhi1K5xqiqgq6tX6A65Iv3gXMAWtxh:ovoNbJYrM4aPZetTdvdkzj9RVTovfqaFtZhkjCIBxJE");
+        
+        return curl_exec($ch);
+    }
 ?>
