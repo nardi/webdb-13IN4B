e87ad672aef0ce15fb6f3ed7ba2c5a0955778674
Nardi Lam <me@nardilam.nl>
2013-01-17 20:35:45 +0100

    Categoriepagina omgezet naar algemeen filter

---
+++ products.php
@@ -0,0 +1,58 @@
+<?php
+    $db = connect_to_db();
+	
+    $query = "SELECT id, titel, prijs FROM Producten";
+    
+    if(isset($_GET['genres']) || isset($_GET['platforms']))
+        $query .= " WHERE";
+    
+    if (isset($_GET['genres']))
+    {
+        $query .= " (genre_id = ";
+        $query .= implode(explode(db->escape_string($_GET['genres']), ','), " OR genre_id = ");
+    }
+    
+    if (isset($_GET['platforms']))
+    {
+        if (isset($_GET['genres']))
+            $query .= ") AND";
+        $query .= " (platform_id = ";
+        $query .= implode(explode($db->escape_string($_GET['platforms']), ','), " OR platform_id = ");
+    }
+
+    $query .= ")";
+    
+    $result = $db->query($query);
+?>
+
+<div id="products">
+
+<div class="category">
+
+<h1><?php echo $genre; ?></h1>
+<?php
+    while ($row = $result->fetch_assoc())
+    {
+        $id = $row['id'];
+        $titel = $row['titel'];
+        $prijs = $row['prijs'];
+?>
+
+<div class="product-thumb">
+    <a href="product.php?id=<?php echo $id; ?>">
+    <img src="images/products/<?php echo $id; ?>.jpg" />
+    <p class="title"><?php echo $titel; ?></p>
+    <p class="price">&euro;<?php echo $prijs; ?></p>
+    </a>
+</div>
+<?php
+    }
+?>
+
+</div>
+
+</div>
+
+<?php
+    $db->close();
+?>
