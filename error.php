5f9f27227d1cf52095a643323c8ca428c73c2385
MetalPinguin <gitpinguin@gmail.com>
2013-01-17 10:28:21 +0100

    error.php toegevoegd, je kan een id meegeven met een foutmelding erin.

---
+++ error.php
@@ -0,0 +1,22 @@
+<pre>
+
+
+            _.._
+           (_.-.\
+       .-,       `
+  .--./ /     _.-""-.
+   '-. (__..-"       \
+      \          a    |
+       ',.__.   ,__.-'/
+         '--/_.'----'`
+Whale, whale, whale. What do we have here?		
+	</pre>
+    
+<?php $ID= $_GET("id");
+if($ID==null)
+    echo "Oeps! Iets ging fout!";
+
+else
+    echo $ID;
+?>
+    
