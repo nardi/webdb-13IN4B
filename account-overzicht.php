53b5d04968170dd66b103145fe2ddcb78336ed37
Abdel Aberkane <a.j.aberkane@gmail.com>
2013-01-17 16:43:34 +0100

    account overzicht

--- account-overzicht.php
+++ account-overzicht.php
@@ -20,7 +20,7 @@ U bent niet ingelogd!
         if (!$sql->fetch()) { print "Onverwachte fout: Geen data."; exit(); }
         $sql->free_result();
 
-        $sql = $db->prepare("SELECT postcode, huisnummer, toevoeging, straat, plaats FROM Adressen JOIN 'Adres_Gebruiker' ON Adressen.id = adres_id WHERE id= '".$_SESSION['gebruiker-id']."' LIMIT 1");
+        $sql = $db->prepare("SELECT postcode, huisnummer, toevoeging, straat, plaats FROM Adressen JOIN 'Adres_Gebruiker' ON Adressen.id = adres_id WHERE gebruiker_id= '".$_SESSION['gebruiker-id']."' LIMIT 1");
         $sql->execute();  
         $sql->bind_result($postcode, $huisnummer, $toevoeging, $straat, $plaats); 
         
