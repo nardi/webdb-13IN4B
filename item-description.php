cef5f4e908f53bdaf57d34f8fffae9d91fdee3dd
MetalPinguin <gitpinguin@gmail.com>
2013-01-18 10:25:58 +0100

    Bezig met dynamische product pagina.Verwijzing in Frontpage aangepast naar .php ipv .html

--- item-description.php
+++ item-description.php
@@ -19,7 +19,7 @@
     $sqli_product->bind_result($titel,$platform,$genre,$beschrijving,$prijs,$release,$voorraad,$toegevoegd);
     if(!$sqli_product->execute())
         throw new Exception($sqli_product->error);
-    
+    $sqli_product->fetch();
     //$sqli_quotes = $db->prepare("SELECT tekst FROM Reviews WHERE product_id=1");
     
     /*$sqli_quotes->bind_param('i',$id);
