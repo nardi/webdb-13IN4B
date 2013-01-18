b64c365f3ca8cf8dd327a31f3f9119227e13175d
Abdel Aberkane <a.j.aberkane@gmail.com>
2013-01-18 12:00:44 +0100

    account-bewerken-test

--- account-bewerken-test.php
+++ account-bewerken-test.php
@@ -12,37 +12,10 @@
     $telefoonnummer2 = $_POST['telefoonnummer2'];
     $telelfoonnummerTot;
     $emailadres = $_POST['e-mailadres'];
-    $wachtwoord = $_POST['wachtwoord'];
     
     $adres_info = json_decode(get_address($postcode, $huisnummer));
     $straat = $adres_info->street;
     $plaats = $adres_info->city;
-    
-    $registratiedatum = date('Y-m-d');
-    
-    //Random getal voor salt genereren
-    $saltbytes = openssl_random_pseudo_bytes(32);
-    $salt = bin2hex($saltbytes);
-    
-    //Hashen met SHA-256
-    $wwhash = hash('sha256', $wachtwoord);
-    $saltedwwhash = hash('sha256', $salt . $wwhash);
-    
-    //Combinatie salt en wachtwoordhash voor database
-    $saltww = $salt . $saltedwwhash;
-    /*               '.       
-        .-""-._     \ \.--|  
-       /       "-..__) .-'   
-     ?_______?             /     
-      \'-.__,   .__.,'       
-       `'----'._\--'  
-     * Whale whale whale, what have we here?
-     *
-     */
-    //Onderstaande is nog niet helemaal af.
-    /*$sql_gebruikers = "INSERT INTO Gebruikers (naam, achternaam, telefoonnummer, email, wachtwoord,
-    registratie_datum, status)
-    VALUES ('$voornaam', '$achternaam', '$telefoonnummer', '$emailadres', '$saltww', '$registratiedatum', '1')";*/
     
     $validNaam = '/^[a-z]{1,256}$/i';
     $validPostcode = '/^[0-9]{4}[\s-]?[a-z]{2}$/i';
@@ -62,11 +35,10 @@
        preg_match($validMail, $emailadres)){
         $telefoonnummerTot = $telefoonnummer . '-' . $telefoonnummer2;
             
-        $sqli_gebruikers = $db->prepare("UPDATE Gebruikers (naam, achternaam, telefoonnummer, email, wachtwoord,
-        registratie_datum, status)
-        VALUES (?,?,?,?,?,?,'1') WHERE id= '".$_SESSION['gebruiker-id']."' ");
+        $sqli_gebruikers = $db->prepare("UPDATE Gebruikers (naam, achternaam, telefoonnummer, email)
+        VALUES (?,?,?,?,?) WHERE id= '".$_SESSION['gebruiker-id']."' ");
 
-        $sqli_gebruikers->bind_param('ssssss',$voornaam, $achternaam, $telefoonnummerTot, $emailadres, $saltww, $registratiedatum);
+        $sqli_gebruikers->bind_param('ssss',$voornaam, $achternaam, $telefoonnummerTot, $emailadres);
         
         /*$sql_adressen = "INSERT INTO Adressen (postcode, huisnummer, toevoeging, plaats, straat)
         VALUES ('$postcode' , '$huisnummer' , '$toevoeging' , '$plaats' , '$straat')";*/
@@ -102,7 +74,6 @@
        preg_match($validHuis, $huisnummer).
        preg_match($validTel1, $telefoonnummer).
        preg_match($validTel2, $telefoonnummer2).
-       preg_match($validMail, $emailadres).
-       preg_match($validWachtwoord, $wachtwoord);
+       preg_match($validMail, $emailadres);
     
 ?>
