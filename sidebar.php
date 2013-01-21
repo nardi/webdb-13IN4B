f30c0b31693d9dd26518e9c3c2f48956509047b1
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:10:16 +0100

    Button test 2

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,20 @@
 ﻿
 
-<div id="beginpagina" class="clickable-item" onClick="window.open('/', '_self');">
+<div id="/" class="clickable-item" onClick="window.open('/', '_self');">
     Beginpagina
 </div>
 
-<div id="producten" class="clickable-item" onClick="window.open('products.php', '_self');">
+<div id="/products.php" class="clickable-item" onClick="window.open('products.php', '_self');">
     Producten
 </div>
 
 <div></div>
 
-<div id="overons" class="clickable-item" onClick="window.open('overons.php', '_self');">
+<div id="/overons.php" class="clickable-item" onClick="window.open('overons.php', '_self');">
     Over ons
 </div>
 
-<div id="contact" class="clickable-item" onClick="window.open('overons.php#contact', '_self');">
+<div id="/overons.php#contact" class="clickable-item" onClick="window.open('overons.php#contact', '_self');">
     Contact
 </div>
 
@@ -33,10 +33,10 @@
         if ($_SESSION['gebruiker-status'] == 3) {
             ?>
             <hr>
-            <div id="producttoevoegen" class="clickable-item" onClick="window.open('product-toevoegen.php', '_self');">
+            <div id="/product-toevoegen.php" class="clickable-item" onClick="window.open('product-toevoegen.php', '_self');">
                 Product Toevoegen
             </div>
-            <div id="f-uptest" class="clickable-item" onClick="window.open('upload-test.html', '_self');">
+            <div id="/upload-test.html" class="clickable-item" onClick="window.open('upload-test.html', '_self');">
                 F-UP test
             </div>
             <?php
