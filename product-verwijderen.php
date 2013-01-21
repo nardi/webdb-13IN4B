e95fcbd9052e37ac9dccb89abf942e9b582e9944
MetalPinguin <gitpinguin@gmail.com>
2013-01-21 16:05:52 +0100

    echo vergeten.

--- product-verwijderen.php
+++ product-verwijderen.php
@@ -4,10 +4,9 @@
         $db = connect_to_db();
         $sqli_preserved_product = $db->prepare("SELECT * FROM Producten WHERE id=?");
         $sqli_preserved_product->bind_param('i',$id);
+        $sqli_preserved_product->bind_result($useless,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
         $sqli_preserved_product->execute();
-        while($sqli_preserved_product->fetch()){
-            $sqli_preserved_product->bind_result($useless,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
-        }
+        $sqli_preserved_product->fetch();
         
         $sqli_preserved_product->free_result();
         
