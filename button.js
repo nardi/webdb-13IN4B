83cadeced900a1fcf22e69cf372f4aa2d12d9451
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:26:34 +0100

    Bug in js, test

--- button.js
+++ button.js
@@ -1,8 +1,8 @@
 function setButtonColor(button) {
-    if(window.location.hash) && (button == "/overons.php") {
+    //if(window.location.hash) && (button == "/overons.php") {
         //button = button + window.location.hash;
-        alert(button);
-    }
+     //   alert(button);
+    //}
     document.getElementById(button).style.background = '#08C1FF';
     document.getElementById(button).style.color = '#FFFFFF';
     document.getElementById(button).style.fontWeight="bold";
