9b7fdd4035dce36441125c0bcb01ce0b0ed33095
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:49:13 +0100

    Button test - werkt wss niet

--- button.js
+++ button.js
@@ -1,10 +1,12 @@
 function onButtonClick(button) {
 
+    if (!window.location.hash) {
+        window.location = button;
+    }
+
     document.getElementById(button).style.background = '#08C1FF';
     document.getElementById(button).style.color = '#FFFFFF';
     document.getElementById(button).style.fontWeight="bold";
     
-    if (!window.location.hash) {
-        window.location = button;
-    }
+    
 }
