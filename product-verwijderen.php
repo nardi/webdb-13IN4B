3ab79202191ed03f9aa33a6bb475dc7d7a67b661
MetalPinguin <gitpinguin@gmail.com>
2013-01-21 16:02:32 +0100

    echo vergeten.

--- product-verwijderen.php
+++ product-verwijderen.php
@@ -9,10 +9,10 @@
             $sqli_preserved_product->bind_result($useless,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
         }
         
+        $sqli_preserved_product->free_result();
+        
         $sqli_preserved_product = $db->prepare("INSERT INTO Producten_Prullenbak VALUES ($useless,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover)");
         $sqli_preserved_product->execute();
-        
-        $sqli_preserved_product->free_result();
         
         $sqli_destroy_product = $db->prepare("DELETE FROM Producten WHERE id=?");
         $sqli_destroy_product->bind_param('i',$id);
