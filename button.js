0e9d81a64693f75f87c041da78b422869619ec3e
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:23:08 +0100

    Alert toegevoegd om te etsten

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
 function setButtonColor(button) {
     if(window.location.hash) && (button == "/overons.php") {
         button = button + window.location.hash;
+        alert(button);
     }
     document.getElementById(button).style.background = '#08C1FF';
     document.getElementById(button).style.color = '#FFFFFF';
