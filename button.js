2425feba916e41385e8844c73b719139ec965d5b
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:11:51 +0100

    Kleuren perongeluk omgewisseld...

--- button.js
+++ button.js
@@ -1,5 +1,5 @@
 function setButtonColor(button) {
-    document.getElementById(button).style.color = '#08C1FF';
-    document.getElementById(button).style.background = '#FFFFFF';
+    document.getElementById(button).style.background = '#08C1FF';
+    document.getElementById(button).style.color = '#FFFFFF';
     document.getElementById(button).style.fontWeight="bold";
 }
