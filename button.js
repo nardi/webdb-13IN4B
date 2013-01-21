34f1256f2ad9997b2f9f703b8abd081efde45ec8
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:48:06 +0100

    Button code herschreven

--- button.js
+++ button.js
@@ -1,9 +1,10 @@
-function setButtonColor(button) {
-    if ((window.location.hash)  && (button == "/overons.php")) {
-        button = button + window.location.hash;
-        alert(button);
-    }
+function onButtonClick(button) {
+
     document.getElementById(button).style.background = '#08C1FF';
     document.getElementById(button).style.color = '#FFFFFF';
     document.getElementById(button).style.fontWeight="bold";
+    
+    if (!window.location.hash) {
+        window.location = button;
+    }
 }
