eec211d2500043094d0b86fa0036775168188864
MetalPinguin <gitpinguin@gmail.com>
2013-01-21 16:40:17 +0100

    echo vergeten.

--- product-verwijderen.php
+++ product-verwijderen.php
@@ -11,7 +11,7 @@
         $sqli_preserved_product->free_result();
         
         $sqli_preserved_product = $db->prepare("INSERT INTO Producten_Prullenbak VALUES (?,?,?,?,?,?,?,?,?,?)");
-        $sqli_preserved_product->bind_param($useless,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
+        $sqli_preserved_product->bind_param('ssssssssss', $useless, $titel, $platform_id, $genre_id, $beschrijving, $prijs, $release_date, $voorraad, $datum_toegevoegd, $cover);
         
         $sqli_preserved_product->execute();
         
