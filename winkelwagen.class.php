8730f54f69d5ced0a3edf07ef1d344c0df9bb949
Nardi Lam <me@nardilam.nl>
2013-01-21 14:25:51 +0100

    Dynamische winkelwagen toegevoegd

--- winkelwagen.class.php
+++ winkelwagen.class.php
@@ -146,9 +146,9 @@ class Winkelwagen
             <td class="product-id"><a href="item-description.php?id=<?php echo $id; ?>"><span name="product-id"><?php echo $id; ?></span></a></td>
             <td class="product-image"><a href="item-description.php?id=<?php echo $id; ?>"><?php echo '<img src="data:image/jpeg;base64,'.base64_encode($cover).'" />';?></a></td>
             <td class="product-title"><a href="item-description.php?id=<?php echo $id; ?>"><?php echo $titel; ?></a></td>
-            <td>&euro;<?php echo $prijs; ?></td>
-            <td><input type="text" class="product-amount" id="amount-<?php echo $id; ?>" value="<?php echo $hoeveelheid; ?>" <?php if (!$editable) echo 'disabled="disabled"'; else echo 'onchange="changeAmount(' . $id . ');'; ?>/></td>
-            <td>&euro;<span id="price-<?php echo $id; ?>"><?php echo $productprijs; ?></span></td>
+            <td>&euro;<span id="price-<?php echo $id; ?>"><?php echo $prijs; ?></span></td>
+            <td><input type="text" class="product-amount" id="amount-<?php echo $id; ?>" value="<?php echo $hoeveelheid; ?>" <?php if (!$editable) echo 'disabled="disabled"'; else echo 'onchange="changeAmount(' . $id . ');'; ?> /></td>
+            <td>&euro;<span id="productprice-<?php echo $id; ?>"><?php echo $productprijs; ?></span></td>
         </tr>
 <?php
             $totaalprijs += $productprijs;
