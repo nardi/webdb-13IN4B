ef79477ca797961f5e934261f510347bd61c7f31
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-21 12:21:19 +0100

    Check voor #contact toegevoegd

--- button.js
+++ button.js
@@ -1,4 +1,7 @@
 function setButtonColor(button) {
+    if(window.location.hash) && (button == "/overons.php") {
+        button = button + window.location.hash;
+    }
     document.getElementById(button).style.background = '#08C1FF';
     document.getElementById(button).style.color = '#FFFFFF';
     document.getElementById(button).style.fontWeight="bold";
