65c020628d60ecbe964db9bed0280db6a8ef7b7c
MetalPinguin <gitpinguin@gmail.com>
2013-01-22 12:58:16 +0100

    Producten beheren pagina toegvoegd.

--- producten-beheren.php
+++ producten-beheren.php
@@ -19,7 +19,7 @@
         $sqli_platform_naam = $db2->prepare("SELECT naam FROM Platforms WHERE id=?");
         $sqli_platform_naam->bind_param('i',$platform_id);
         $sqli_platform_naam->bind_result($platformnaam);
-        echo "$platformnaam";
+        echo "$platformnaam . FAILED!";
         $sqli_platform_naam->execute();
         
         
