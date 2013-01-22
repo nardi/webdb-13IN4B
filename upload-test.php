201a087ff1f1c8a971b428794297315187aee0b5
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-22 11:18:48 +0100

    Upload test

--- upload-test.php
+++ upload-test.php
@@ -1,12 +1,38 @@
 <?php
-$folder = "/htdocs/uploads/";
-if (is_uploaded_file($HTTP_POST_FILES['filename']['tmp_name']))  {   
-    if (move_uploaded_file($HTTP_POST_FILES['filename']['tmp_name'], $folder.$HTTP_POST_FILES['filename']['name'])) {
-         Echo "File uploaded";
-    } else {
-         Echo "File not moved to destination folder. Check permissions";
-    };
-} else {
-     Echo "File is not uploaded.";
-}; 
+$allowedExts = array("jpg", "jpeg", "gif", "png");
+$extension = end(explode(".", $_FILES["file"]["name"]));
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 20000)
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("/uploads/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "/uploads/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "/uploads/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
 ?>
