9612412bab729c8fccc570d3f19198904d2f251b
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-22 11:39:37 +0100

    Upload test

--- upload-test.php
+++ upload-test.php
@@ -27,7 +27,7 @@ if ((($_FILES["file"]["type"] == "image/gif")
     else
       {
       move_uploaded_file($tmpdir . $_FILES["file"]["tmp_name"],
-      "/uploads/" . $_FILES["file"]["name"]);
+      dirname(__FILE__) . "/uploads/" . $_FILES["file"]["name"]);
       echo "Stored in: " . "/uploads/" . $_FILES["file"]["name"];
       }
     }
