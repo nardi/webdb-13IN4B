def93436e1891870ec0cbf485a84cbd3667aea28
MetalPinguin <gitpinguin@gmail.com>
2013-01-22 13:26:45 +0100

    Producten beheren pagina toegvoegd.

--- producten-beheren.php
+++ producten-beheren.php
@@ -3,10 +3,10 @@
 <?php
     $db = connect_to_db();
     
-    $sqli_product_lijst = $db->prepare("SELECT * FROM Producten");
+   /* $sqli_product_lijst = $db->prepare("SELECT * FROM Producten");
     $sqli_product_lijst->bind_result($id,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
     $sqli_product_lijst->execute();
-    
+    */
     
 ?>
 <table id="Producten">
