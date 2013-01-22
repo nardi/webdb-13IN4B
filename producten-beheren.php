0032a38c347162028f4e9a315c024f5dda3f9c31
MetalPinguin <gitpinguin@gmail.com>
2013-01-22 16:33:37 +0100

    Edit functie in tabel gezet in Producten beheren.

--- producten-beheren.php
+++ producten-beheren.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="main.css">
+<script src="productenBeheren.js"></script>
 
 <?php
     $db = connect_to_db();
@@ -10,14 +11,36 @@
     
 ?>
 <table id="Producten">
-<tr><th>id</th><th>Titel</th><th>Cover</th><th>Platform</th><th>Genre</th><th>Prijs</th><th>Voorraad</th>
+<tr><th>Geslecteerd</th><th>Product Nummer</th><th>Titel</th><th>Cover</th><th>Platform</th><th>Genre</th><th>Prijs</th><th>Voorraad</th>
 
+<form name='EditProduct' id='EditProductId' onsubtmit='verander-product.php' method='post'>
 <?php
     $db2 = connect_to_db();
     $db3 = connect_to_db();
+    
     while($sqli_product_lijst->fetch()){        
-        $cover_var = '<img src="data:image/jpeg;base64,'.base64_encode($cover).'"/>';
-        
-        echo "<tr><td class=column>$id</td><td class=column>$titel</td><td class=column><div class='cover'>$cover_var</td></div><td class=column>$platformnaam</td><td class=column>$genrenaam</td><td class=column>$prijs</td><td class=column>$voorraad</td></tr>";
+        $cover_var = '<img src="$imagedir.$cover."/>';
+        echo "<tr id=$id>
+            <td><input type='checkbox' name='selected'></td>
+            <td class='column'><input type='text' id='product_id'.$id name='product_nummer' disabled='disabled' value=$id></td>
+            <td class='column'><input type='text' id='titel_id'.$id name='titel' disabled='disabled' value=$titel></td>
+            <td class='column'><div class='cover' value=$cover_var></td></div>
+            <td class='column'><input type='text' id='platform_id'.$id name='platform' disabled='disabled' value=$platformnaam></td>
+            <td class='column'><input type='text' id='genre_id'.$id name='genre' disabled='disabled' value=$genrenaam></td>
+            <td class='column'><input type='text' id='prijs_id'.$id name='prijs' disabled='disabled' value=$prijs></td>
+            <td class='column'><input type='text' id='voorraad_id'.$id name='voorraad' disabled='disabled' value=$voorraad></td>
+            <td>
+                <div class='ProductEdit' onclick='EnableEdit($id)'>
+                </div>
+            </td>
+            <td>
+                <div class='ProductDelete>
+                        <input type='hidden' name='delete' value='$id'>
+                        <input type='submit' value='' name='submitButton' class='DeleteSubmitButton'.$id>
+                </div>
+            </td>
+            ";
+        </tr>";
     }
 ?>
+</form>
---
+++ productenBeheren.js
@@ -0,0 +1,4 @@
+function enableEdit(id){
+    var trId=document.getElementById("id");
+    trId.getElementsByTagName("input").disabled = false;
+}
