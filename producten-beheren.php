8a4688063a78da7f48dec088e1acf8a95c0117cf
MetalPinguin <gitpinguin@gmail.com>
2013-01-22 14:04:44 +0100

    3 queries in 1 door JOIN in producten-beheren.php.

--- producten-beheren.php
+++ producten-beheren.php
@@ -3,8 +3,8 @@
 <?php
     $db = connect_to_db();
     
-    $sqli_product_lijst = $db->prepare("SELECT * FROM Producten");
-    $sqli_product_lijst->bind_result($id,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
+    $sqli_product_lijst = $db->prepare("SELECT id,titel,platform_id,genre_id,beschrijving,prijs,release_date,voorraad,datum_toegevoegd,cover,Platforms.naam,Genres.naam FROM Producten JOIN Producten ON platform_id=Platforms.id JOIN Genres ON genre_id=Genres.naam");
+    $sqli_product_lijst->bind_result($id,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover,$platformnaam,$genrenaam);
     $sqli_product_lijst->execute();
     
     
@@ -15,21 +15,7 @@
 <?php
     $db2 = connect_to_db();
     $db3 = connect_to_db();
-    while($sqli_product_lijst->fetch()){
-        $sqli_platform_naam = $db2->prepare("SELECT naam FROM Platforms WHERE id=?");
-        $sqli_platform_naam->bind_param('i',$platform_id);
-        $sqli_platform_naam->bind_result($platformnaam);
-        echo "$platformnaam . FAILED! . $platform_id";
-        
-        if(!$sqli_platform_naam->execute())
-            $db2->error;
-        
-        
-        $sqli_genre_naam = $db3->prepare("SELECT naam FROM Genres WHERE id=?");
-        $sqli_genre_naam->bind_param('i',$genre_id);
-        $sqli_genre_naam->bind_result($genrenaam);
-        $sqli_genre_naam->execute();
-        
+    while($sqli_product_lijst->fetch()){        
         $cover_var = '<img src="data:image/jpeg;base64,'.base64_encode($cover).'"/>';
         
         echo "<tr><td class=column>$id</td><td class=column>$titel</td><td class=column><div class='cover'>$cover_var</td></div><td class=column>$platformnaam</td><td class=column>$genrenaam</td><td class=column>$prijs</td><td class=column>$voorraad</td></tr>";
