9edb1d34aa7da307bb2b634f48d252294b22a4bd
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-22 10:50:55 +0100

    Bug in uploaden gefixed

--- upload-test.php
+++ upload-test.php
@@ -12,16 +12,16 @@ else
   }
   
   
-  if (file_exists("upload/" . $_FILES["file"]["name"]))
+  if (file_exists("uploads/" . $_FILES["file"]["name"])) {
       {
       echo $_FILES["file"]["name"] . " already exists. ";
       }
     else
       {
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      "uploads/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "uploads/" . $_FILES["file"]["name"];
       }
-    }
+   }
 ?> 
  
