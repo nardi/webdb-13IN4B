d6b7d54365cf2ccf0f640e31c931e67c515f5072
Nardi Lam <me@nardilam.nl>
2013-01-22 12:30:13 +0100

    Dynamischewinkelwagenproblemen

--- backend/hoeveelheden.php
+++ backend/hoeveelheden.php
@@ -6,7 +6,12 @@
     foreach ($ww->get_all() as $id)
     {
         if (isset($_POST["amount-$id"]))
+        {
             $ww->change_amount($id, $_POST["amount-$id"]);
+            echo "amount-$id changed";
+        }
+        else
+            echo "amount-$id not changed";
     }
     
     $ww->save_to_session();
