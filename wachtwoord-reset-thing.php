2a68f972f5368b2efea77e606321f01c9d97ec48
Stefani <kirakiraboshi82@gmail.com>
2013-01-23 12:42:09 +0100

    kleine bug

---
+++ wachtwoord-reset-thing.php
@@ -0,0 +1,12 @@
+
+
+<div align='justify'>
+    Vul hieronder het door u nieuwe gekozen wachtwoord in. 
+    </div><br /><br />";
+	echo "<form method='post' action='wachtwoord-reset.php'>
+	  
+      Wachtwoord: <input name='wachtwoord' type='text'><br />
+	  Wachtwoord nogmaals: <input name='wachtwoord_nogmaals' type='text'><br />
+      </textarea><br>
+      <input type='submit'>
+      </form>";
--- wachtwoord-reset.php
+++ wachtwoord-reset.php
@@ -21,7 +21,7 @@ if (!isset($_POST['wachtwoord'])&&
     echo "<div align='justify'>
     Vul hieronder het door u nieuwe gekozen wachtwoord in. 
     </div><br /><br />";
-	echo "<form method='post' action='wachtwoord-reset-thing.php'>
+	echo "<form method='post' action='wachtwoord-reset.php'>
 	  <input name='token' type='hidden' value='$token'>
       Wachtwoord: <input name='wachtwoord' type='text'><br />
 	  Wachtwoord nogmaals: <input name='wachtwoord_nogmaals' type='text'><br />
