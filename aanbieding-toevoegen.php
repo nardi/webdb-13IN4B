abcc0c170a5bd90ccf61baab20528d7202634118
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-23 15:29:59 +0100

    Aanbiedingen kunnen nu aangemaakt worden, nog niet aan de database toegevoegd worden

---
+++ aanbieding-toevoegen.php
@@ -0,0 +1,43 @@
+<?php
+    $db = connect_to_db();
+    ?>
+   
+    
+    <div class="centered-container">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <div align="right"> 
+                <h1><center><b>Aanbieding toevoegen</b></center></h1>
+                <hr width="100%">
+				<center><b>Aanbiedings-gegevens</b></center>
+				<br />
+                Product: 
+                <select name="platform">
+                <?php
+                    $productensql = $db->prepare("SELECT id, titel FROM Producten");
+                    $productensql->bind_result($productid, $product);
+                    $productensql->execute();
+                    
+                    while ($platformsql->fetch()) {
+                ?>
+                        <option value="<?php echo $productid; ?>"><?php echo $product; ?></option>
+                <?php
+                    }
+                    $productensql->free_result();
+                    $db->close();
+                ?>
+                </select> <br />
+                
+                Aanbiedingsprijs: <input type="text" name="prijs"><br />
+                Begin-datum: <input type="text" name="release_date"><br />
+                Eind-datum: <input type="text" name="release_date"><br />
+                
+                <input type="submit" value="Voeg aanbieding toe">
+       
+                
+                
+                
+            </div>
+        </form>
+    </div>
+<?php
+?>
