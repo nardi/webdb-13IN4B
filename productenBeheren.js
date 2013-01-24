560496d98c1b8a47be3791878d1f16a2325067e7
MetalPinguin <gitpinguin@gmail.com>
2013-01-24 14:55:04 +0100

    PLaatje veranderd in upload knop?

--- productenBeheren.js
+++ productenBeheren.js
@@ -2,8 +2,15 @@ function enableEdit(id){
     var trId = document.getElementById(id);
     var array = trId.getElementsByTagName("input")
     var arrayLength = array.length;
+    var coverId = document.getElementsById("cover"+id);
     
     for(i=0;i<arrayLength;i++){
         array[i].disabled = false;
     }
+    
+    coverId.onclick="uploadCover(coverId)";
+}
+
+function uploadCover(coverId){
+    coverId.innerHTML="<input type='file' name='image$id'>";
 }
