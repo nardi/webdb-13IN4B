3b79eeafed87963b9c0b15894403dd84f18347c8
Stefani <kirakiraboshi82@gmail.com>
2013-01-24 11:44:49 +0100

    registratiemail update

---
+++ registratie-geverifieerd.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="registratie-succesvol.css" />
+<link rel="stylesheet" type="text/css" href="registratie-succesvol-classic-theme.css" />
+
+<div class="registratie-succesvol-container">
+    <div id="logo-registratie-succesvol" title="Super Internet Shop">
+    </div>
+    
+    <div class="bedankje">
+        <p class="bedankje"> Bedankt, uw registratie is geverifieerd.</p>
+        <a href="frontpage.html">
+        <p class="bedankje">Klik hier om terug te keren naar het hoofdmenu</p>
+        </a>
+    </div>
+    
+</div>
