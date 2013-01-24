3ea7f799299a62f674d689fb73f9cc8ce0065797
Nardi Lam <me@nardilam.nl>
2013-01-24 22:28:16 +0100

    Aanbiedingen frontpage (dronken programmeren op station yeah)

--- frontpage.php
+++ frontpage.php
@@ -1,59 +1,44 @@
 <div id="frontpage">
 <?php
     $db = connect_to_db();
+    
+    $on_sale = $db->prepare('SELECT id, titel, Producten.prijs, Aanbiedingen.prijs, cover FROM Producten JOIN Aanbiedingen ON product_id = Producten.id WHERE start_datum <= CURRENT_DATE AND eind_datum >= CURRENT_DATE LIMIT 8');
+    $on_sale->bind_result($id, $titel, $oude_prijs, $prijs, $cover);
+    $on_sale->execute();
+    if ($on_sale->fetch())
+    {
 ?>
 
 <div id="on-sale" class="category">
 
 <h1>In de aanbieding</h1>
+<?php
+        do
+        {
+?>
 
 <div class="product-hilite">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
+    <a href="item-description.php?id=<?php echo $id; ?>">
+    <img src="<?php echo is_valid_cover($cover); ?>" />
     <div id="desc" class="vcenter-container">
         <div class="vcenter">
-            <p class="title">Battletoads</p>
+            <p class="title"><?php echo $titel; ?></p>
             <p>van</p>
-            <p class="old-price">&euro;42,-</p>
+            <p class="old-price">&euro;<?php echo $oude_prijs; ?></p>
             <p>voor</p>
-            <p class="price">&euro;13,37</p>
+            <p class="price">&euro;<?php echo $prijs; ?></p>
         </div>
     </div>
     </a>
 </div>
-
-<div class="product-hilite">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <div id="desc" class="vcenter-container">
-        <div class="vcenter">
-            <p class="title">Battletoads</p>
-            <p>van</p>
-            <p class="old-price">&euro;42,-</p>
-            <p>voor</p>
-            <p class="price">&euro;13,37</p>
-        </div>
-    </div>
-    </a>
-</div>
-
-<div class="product-hilite">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <div id="desc" class="vcenter-container">
-        <div class="vcenter">
-            <p class="title">Battletoads</p>
-            <p>van</p>
-            <p class="old-price">&euro;42,-</p>
-            <p>voor</p>
-            <p class="price">&euro;13,37</p>
-        </div>
-    </div>
-    </a>
-</div>
+<?php
+        while($on_sale->fetch());
+?>
 
 </div>
 <?php
+    }
+
     $new_releases = $db->prepare('SELECT id, titel, prijs, cover FROM Producten WHERE release_date < CURRENT_DATE ORDER BY release_date DESC LIMIT 8');
     $new_releases->bind_result($id, $titel, $prijs, $cover);
     $new_releases->execute();
