1cdae84c167f61d80f6394d6cac24499e5f9b861
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-24 09:29:37 +0100

    Aanbiedingen kunnen nu, als het goed is, ook in de database worden opgeslagen

---
+++ aanbieding-toevoegen-db.php
@@ -0,0 +1,26 @@
+<?php
+    if (is_admin())
+    {
+        $db = connect_to_db();
+        $productid = $_POST['producten'];
+        $prijs = $_POST['prijs'];
+        $startdatum = $_POST['begin-datum'];
+        $einddatum = $_POST['eind-datum'];
+       
+        $sqli_producten = $db->prepare("INSERT INTO Aanbiedingen (product-id, prijs, start-datum, eind-datum)
+        VALUES (?,?,?,?)");
+        $sqli_producten->bind_param('isss', $productid, $prijs, $startdatum, $einddatum);
+        if(!$sqli_producten->execute())
+            throw new Exception($sqli_producten->error);
+
+        $db->close()
+        
+        redirect_to("product-toevoegen-succesvol.html");
+        ?>
+        
+        <script type="text/javascript">
+            alert("Aanbieding succesvol toegevoegd");
+        </script>    
+     <?php
+     }
+?>
--- aanbieding-toevoegen.php
+++ aanbieding-toevoegen.php
@@ -1,43 +1,46 @@
 <?php
-    $db = connect_to_db();
-    ?>
-   
-    
-    <div class="centered-container">
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <div align="right"> 
-                <h1><center><b>Aanbieding toevoegen</b></center></h1>
-                <hr width="100%">
-				<center><b>Aanbiedings-gegevens</b></center>
-				<br />
-                Product: 
-                <select name="producten">
-                <?php
-                    $productensql = $db->prepare("SELECT id, titel FROM Producten");
-                    $productensql->bind_result($productid, $product);
-                    $productensql->execute();
-                    
-                    while ($productensql->fetch()) {
-                ?>
-                        <option value="<?php echo $productid; ?>"><?php echo $product; ?></option>
-                <?php
-                    }
-                    $productensql->free_result();
-                    $db->close();
-                ?>
-                </select> <br />
-                
-                Aanbiedingsprijs: <input type="text" name="prijs"><br />
-                Begin-datum: <input type="text" name="release_date"><br />
-                Eind-datum: <input type="text" name="release_date"><br />
-                
-                <input type="submit" value="Voeg aanbieding toe">
+    if (is_admin())
+    {
+        $db = connect_to_db();
+        ?>
        
-                
-                
-                
-            </div>
-        </form>
-    </div>
-<?php
+        
+        <div class="centered-container">
+            <form action="aanbieding-toevoegen-db.php" method="post">
+                <div align="right"> 
+                    <h1><center><b>Aanbieding toevoegen</b></center></h1>
+                    <hr width="100%">
+                    <center><b>Aanbiedings-gegevens</b></center>
+                    <br />
+                    Product: 
+                    <select name="producten">
+                    <?php
+                        $productensql = $db->prepare("SELECT id, titel FROM Producten");
+                        $productensql->bind_result($productid, $product);
+                        $productensql->execute();
+                        
+                        while ($productensql->fetch()) {
+                    ?>
+                            <option value="<?php echo $productid; ?>"><?php echo $product; ?></option>
+                    <?php
+                        }
+                        $productensql->free_result();
+                        $db->close();
+                    ?>
+                    </select> <br />
+                    
+                    Aanbiedingsprijs: <input type="text" name="prijs"><br />
+                    Begin-datum: <input type="text" name="begin-datum"><br />
+                    Eind-datum: <input type="text" name="eind-datum"><br />
+                    
+                    <input type="submit" value="Voeg aanbieding toe">
+           
+                    
+                    
+                    
+                </div>
+            </form>
+        </div>
+    <?php
+    }
 ?>
