d339a7d1ab7a80f49d04fe05a47935a389f560a7
Nardi Lam <me@nardilam.nl>
2013-01-24 15:40:56 +0100

    nieuwe releases frontpage

--- frontpage.php
+++ frontpage.php
@@ -53,69 +53,38 @@
 </div>
 
 </div>
+<?php
+    $new_releases = $db->prepare('SELECT id, titel, prijs, cover FROM Producten WHERE release_date < CURRENT_DATE ORDER BY release_date DESC LIMIT 8');
+    $new_releases->bind_result($id, $titel, $prijs, $cover);
+    $new_releases->execute();
+    if ($new_releases->fetch())
+    {
+?>
 
 <div id="new-releases" class="category">
 
 <h1>Nieuwe releases</h1>
+<?php
+        do
+        {
+?>
 
 <div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
+    <a href="item-description.php?id=<?php echo $id; ?>">
+    <img src="<?php echo is_valid_cover($cover); ?>" />
+    <p class="title"><?php echo $titel; ?></p>
+    <p class="price">&euro;<?php echo $prijs; ?></p>
     </a>
 </div>
-
-<div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
-    </a>
-</div>
-
-<div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
-    </a>
-</div>
-
-<div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
-    </a>
-</div>
-
-<div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
-    </a>
-</div>
-
-<div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
-    </a>
-</div>
-
-<div class="product-thumb">
-    <a href="item-description.php?id=1">
-    <img src="images/products/1.jpg" />
-    <p class="title">Battletoads</p>
-    <p class="price">&euro;42,-</p>
-    </a>
-</div>
+<?php
+        } while ($new_releases->fetch());
+?>
 
 </div>
 <?php
+    }
+    $new_releases->free_result();
+
     $pre_orders = $db->prepare('SELECT id, titel, prijs, release_date, cover FROM Producten WHERE release_date > CURRENT_DATE LIMIT 8');
     $pre_orders->bind_result($id, $titel, $prijs, $datum, $cover);
     $pre_orders->execute();
@@ -141,12 +110,14 @@
 </div>
 <?php
         } while ($pre_orders->fetch());
+?>
+
+</div>
+<?php  
     }
     $pre_orders->free_result();
     
     $db->close();
 ?>
-
-</div>
 
 </div>
