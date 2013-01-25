eb6d0eb0774a45ddac1b8a63bdaa1899732a940f
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-25 13:49:20 +0100

    Sexify the code, step 1: producst.php

--- products.php
+++ products.php
@@ -1,141 +1,142 @@
 <div class="centered-container">
-<div id="filters">
-    <form method="get" action="products.php">
-        Platform:
-        <div class="platform">
-       
-        <select name="platforms">
+    <div id="filters">
+        <form method="get" action="products.php">
+            Platform:
+            <div class="platform">
+                <select name="platforms">
 
-       <?php
-        $db = connect_to_db();
-        $platformsql = $db->prepare("SELECT id, naam FROM Platforms");
-        $platformsql->execute();
-        $platformsql->bind_result($platformid, $platform);
-        
-        echo "<option value='0'>--- Geen ---</option>";
-        
-        while ($platformsql->fetch()) {
-?>
-						  <option value="<?php echo $platformid; ?>"><?php echo $platform; ?></option>
-<?php
-        }
-        
-        $platformsql->free_result();
-?>
-						  </select>
-					  </div>          
-					  Genre:
-					  <div class="genre">
+                <?php
+                $db = connect_to_db();
+                $platformsql = $db->prepare("SELECT id, naam FROM Platforms");
+                $platformsql->execute();
+                $platformsql->bind_result($platformid, $platform);
+                
+                echo "<option value='0'>--- Geen ---</option>";
+                
+                while ($platformsql->fetch()) {
+                    ?>
+                    <option value="<?php echo $platformid; ?>"><?php echo $platform; ?></option>
+                    <?php
+                }
+                
+                $platformsql->free_result();
+                ?>
+                
+                </select>
+            </div>          
+            
+            Genre:
+            <div class="genre">
 
-						  <select name="genres">
-<?php
-        $genresql = $db->prepare("SELECT id, naam FROM Genres");
-        $genresql->execute();
-        $genresql->bind_result($genreid, $genre);
+                <select name="genres">
+            
+                <?php
+                $genresql = $db->prepare("SELECT id, naam FROM Genres");
+                $genresql->execute();
+                $genresql->bind_result($genreid, $genre);
 
-        echo "<option value='0'>--- Geen ---</option>";
+                echo "<option value='0'>--- Geen ---</option>";
+            
+                while ($genresql->fetch()) {
+                    ?>
+                    <option value="<?php echo $genreid; ?>"><?php echo $genre; ?></option>
+                    <?php
+                }
+            
+                $genresql->free_result();
+                ?>
+                </select>
+            </div>      
+            <br />
         
-        while ($genresql->fetch()) {
-?>
-						  <option value="<?php echo $genreid; ?>"><?php echo $genre; ?></option>
-<?php
-        }
-        
-        $genresql->free_result();
-        
-?>
-                          </select>
-					  </div>      
-    <br />
-    
-    <input type="submit" value="Pas filters toe">
-    </form>
-</div>
-<hr>
+            <input type="submit" value="Pas filters toe">
+        </form>
+    </div>
+    <hr>
 
-<?php
-    function check_array(&$var, $id, $db)
-    {
-        $var = $db->escape_string($var);
-        $var = intval($var);
-    }
-    
-    $query = "SELECT id, titel, prijs, cover FROM Producten";
-    
-    if(isset($_GET['genres']) && $_GET['genres'] != 0) 
-    {
-        $genres_valid = true;
-    }
-    else 
-    {
-        $genres_valid = false;
-    }
-    
-    if(isset($_GET['platforms']) && $_GET['platforms'] != 0) 
-    {
-        $platforms_valid = true;
-    }
-    else 
-    {
-        $platforms_valid = false;
-    }
-    
-    
-    if($genres_valid || $platforms_valid)
-    {
-        $query .= " WHERE";
-    
-        if ($genres_valid)
+    <?php
+        function check_array(&$var, $id, $db)
         {
-            $query .= " (genre_id = ";
-            $genres = explode(',', $_GET['genres']);
-            array_walk($genres, 'check_array', $db);
-            $query .= implode(" OR genre_id = ", array_filter($genres));
+            $var = $db->escape_string($var);
+            $var = intval($var);
         }
         
-        if ($platforms_valid)
+        $query = "SELECT id, titel, prijs, cover FROM Producten";
+        
+        if(isset($_GET['genres']) && $_GET['genres'] != 0) 
         {
-            if ($genres_valid)
-                $query .= ") AND";
-            $query .= " (platform_id = ";
-            $platforms = explode(',', $_GET['platforms']);
-            array_walk($platforms, 'check_array', $db);
-            $query .= implode(" OR platform_id = ", array_filter($platforms));
+            $genres_valid = true;
         }
-
-        $query .= ")";
-    }
-    
-    if (isset($_GET['search']))
-    {
-        $search = '%' . $db->escape_string($_GET['search']) . '%';
-        if (isset($_GET['genres']) || isset($_GET['platforms']))
-            $query .= " AND";
-        else
+        else 
+        {
+            $genres_valid = false;
+        }
+        
+        if(isset($_GET['platforms']) && $_GET['platforms'] != 0) 
+        {
+            $platforms_valid = true;
+        }
+        else 
+        {
+            $platforms_valid = false;
+        }
+        
+        
+        if($genres_valid || $platforms_valid)
+        {
             $query .= " WHERE";
-        $query .= " titel LIKE ?";
-    }
-    
-    $sqli = $db->prepare($query);
-    if (isset($search))
-        $sqli->bind_param('s', $search);
-    $sqli->bind_result($id, $titel, $prijs, $cover);
-    if (!$sqli->execute())
-            throw new Exception("Er zijn foutieve parameters opgegeven.");
-?>
+        
+            if ($genres_valid)
+            {
+                $query .= " (genre_id = ";
+                $genres = explode(',', $_GET['genres']);
+                array_walk($genres, 'check_array', $db);
+                $query .= implode(" OR genre_id = ", array_filter($genres));
+            }
+            
+            if ($platforms_valid)
+            {
+                if ($genres_valid)
+                    $query .= ") AND";
+                $query .= " (platform_id = ";
+                $platforms = explode(',', $_GET['platforms']);
+                array_walk($platforms, 'check_array', $db);
+                $query .= implode(" OR platform_id = ", array_filter($platforms));
+            }
+
+            $query .= ")";
+        }
+        
+        if (isset($_GET['search']))
+        {
+            $search = '%' . $db->escape_string($_GET['search']) . '%';
+            if (isset($_GET['genres']) || isset($_GET['platforms']))
+                $query .= " AND";
+            else
+                $query .= " WHERE";
+            $query .= " titel LIKE ?";
+        }
+        
+        $sqli = $db->prepare($query);
+        if (isset($search))
+            $sqli->bind_param('s', $search);
+        $sqli->bind_result($id, $titel, $prijs, $cover);
+        if (!$sqli->execute())
+                throw new Exception("Er zijn foutieve parameters opgegeven.");
+    ?>
 
 
-<div class="category">
+    <div class="category">
 
-<?php
-    while ($sqli->fetch())
-    {
-        $cover = is_valid_cover($cover);
-        product_thumb($id, $cover, $titel, $prijs);
-    }
- ?>
- 
-</div>
+    <?php
+        while ($sqli->fetch())
+        {
+            $cover = is_valid_cover($cover);
+            product_thumb($id, $cover, $titel, $prijs);
+        }
+     ?>
+     
+    </div>
 
 </div>
 
