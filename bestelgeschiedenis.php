fb1af8898b0ae9ad8516d794ee3d193964050fff
Nardi Lam <me@nardilam.nl>
2013-01-25 16:23:13 +0100

    Datum in bestelgeschiedenis

--- bestelgeschiedenis.php
+++ bestelgeschiedenis.php
@@ -13,11 +13,19 @@
     <div align="left"> 
 <?php
         $db = connect_to_db();
-        $bestellingen = $db->prepare("SELECT id FROM Bestellingen WHERE gebruiker_id = ?");
+        $bestellingen = $db->prepare("SELECT id, timestamp FROM Bestellingen WHERE gebruiker_id = ?");
         $bestellingen->bind_param('i', $_SESSION['gebruiker-id']);
-        $bestellingen->bind_result($bestelling_id);
+        $bestellingen->bind_result($bestelling_id, $timestamp);
         $bestellingen->execute();
         $bestellingen->store_result();
+?>
+    <table style="display: inline-block;">
+        <tr>
+            <th></th>
+            <th>Totaalbedrag</th>
+            <th>Datum</th>
+        </tr>
+<?php
         while ($bestellingen->fetch())
         {
             $producten = $db->prepare("SELECT hoeveelheid, prijs FROM Bestelling_Product WHERE bestelling_id = ?");
@@ -28,13 +36,18 @@
             while ($producten->fetch())
                 $totaalprijs += $hoeveelheid * $prijs;
 ?>
-<a href="bestelling.php?id=<?php echo $bestelling_id; ?>">Bestelling #<?php echo $bestelling_id; ?> met een totaal van &euro;<?php echo $totaalprijs; ?></a><br/>
+        <tr class="clickable-item" onclick="window.location = 'bestelling.php?id=<?php echo $bestelling_id; ?>';">
+            <td>Bestelling #<?php echo $bestelling_id; ?></td>
+            <td>&euro;<?php echo price($totaalprijs); ?></td>
+            <td><?php echo date('d-m-Y', $timestamp); ?></td>
+        </tr>
 <?php
             $producten->free_result();
         }
         $bestellingen->free_result();
         $db->close();
 ?>
+    </table>
     </div>
     <hr width="100%">
     <a href="bestellingen-lopende.html"><input type="submit" value="Lopende bestellingen"></a><br />
