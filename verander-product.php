f175e28b1960cfb0b9152efcd3bb947a9db22c53
MetalPinguin <gitpinguin@gmail.com>
2013-01-25 17:31:44 +0100

    Dingen gefixed

--- verander-product.php
+++ verander-product.php
@@ -5,30 +5,30 @@
     $sqli_id->bind_result($id);
     $sqli_id->execute();
     while($sqli_id->fetch()){
-    
-        $titel=$_POST['titel'.$id];
-        $platform_id=$_POST['platform'.$id];
-        $genre_id=$_POST['genre'.$id];
-        $beschrijving=$_POST['beschrijving'.$id];
-        $prijs=$_POST['prijs'.$id];
-        $release_date=$_POST['release'.$id];
-        $voorraad=$_POST['voorraad'.$id];
-        $cover=upload_image('image'.$id);
-        
-        $sqli_verander = $db->prepare("UPDATE Producten SET titel=?, platform_id=?,genre_id=?,beschrijving=?, prijs=?, release_date=?, voorraad=? WHERE id=?");
-        
-        $sqli_verander->bind_param('siisdsis',$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$id);
-        
-        $sqli_verander->execute();
-        
-        if(isset($cover)){
-            $sqli_verander_cover = $db->prepare("UPDATE Producten SET cover=? WHERE id=?");
-            $sqli_verander_cover->bind_param('ss',$cover,$id);
-            $sqli_verander_cover->execute();
+        if(isset($_POST['titel'.$id]){
+            $titel=$_POST['titel'.$id];
+            $platform_id=$_POST['platform'.$id];
+            $genre_id=$_POST['genre'.$id];
+            $beschrijving=$_POST['beschrijving'.$id];
+            $prijs=$_POST['prijs'.$id];
+            $release_date=$_POST['release'.$id];
+            $voorraad=$_POST['voorraad'.$id];
+            $cover=upload_image('image'.$id);
             
-        }
-        
-        
+            $sqli_verander = $db->prepare("UPDATE Producten SET titel=?, platform_id=?,genre_id=?,beschrijving=?, prijs=?, release_date=?, voorraad=? WHERE id=?");
+            
+            $sqli_verander->bind_param('siisdsis',$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$id);
+            
+            $sqli_verander->execute();
+            
+            if(isset($cover)){
+                $sqli_verander_cover = $db->prepare("UPDATE Producten SET cover=? WHERE id=?");
+                $sqli_verander_cover->bind_param('ss',$cover,$id);
+                $sqli_verander_cover->execute();
+                
+            }
+        }    
+            
 
     }
     $db->close();
