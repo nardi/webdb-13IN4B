0c05b2210324a166a3a7208911ba62dbd1425bfa
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-25 11:35:02 +0100

    Test met exception in acc overzicht

--- account-overzicht.php
+++ account-overzicht.php
@@ -17,7 +17,10 @@ U bent niet ingelogd!
         $sql->execute();  
         $sql->bind_result($naam, $achternaam, $telefoonnummer, $email);
         
-        if (!$sql->fetch()) { print "Onverwachte fout: Geen data."; exit(); }
+        if (!$sql->fetch()) { 
+            //print "Onverwachte fout: Geen data."; exit(); 
+            throw new Exception("Onverwachtse fout: geen data. Neem contact op met de site-beheerder");
+        }
         $sql->free_result();
 
         $sql = $db->prepare("SELECT postcode, huisnummer, toevoeging, plaats, straat FROM Adressen JOIN AdresGebruiker ON Adressen.id = adres_id WHERE gebruiker_id= '".$_SESSION['gebruiker-id']."' LIMIT 1");
