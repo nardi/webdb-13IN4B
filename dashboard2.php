39764e15453fe8e9cb5e489a532f7a90d9aeebdd
Nardi Lam <me@nardilam.nl>
2013-01-27 21:38:51 +0100

    Opmaakdingetjes

--- dashboard2.php
+++ dashboard2.php
@@ -1,12 +1,7 @@
 <?php
-if(!isset($_SESSION['logged-in'])) {
-    ?>
-    <script>
-        function emptyOrDefault(elem,dv) { elem.value = elem.value == '' ? dv : elem.value; }
-    </script>
-    
-    <form action="gebruikers-login.php" method="post">
-        <input type="text" name="e-mailadres" value="E-mailadres" onfocus="value=''" onblur="emptyOrDefault(this, 'E-mailadres')">
+    function ww_dash()
+    {
+?>
         <div id="ww-dash" class="clickable-item" onclick="window.location = 'winkelwagen.php'">
             <?php    
             $ww = Winkelwagen::try_load_from_session();
@@ -18,32 +13,35 @@ if(!isset($_SESSION['logged-in'])) {
                 <?php echo $aantal; ?>
             </div>
         </div>
+<?php
+    }
+    
+    if(!isset($_SESSION['logged-in'])) {
+    ?>
+    <script>
+        function empty(elem,dv) { elem.value = elem.value == dv ? '' : elem.value; }
+        function emptyOrDefault(elem,dv) { elem.value = elem.value == '' ? dv : elem.value; }
+    </script>
+    <form action="gebruikers-login.php" method="post">
+        <input type="text" name="e-mailadres" value="E-mailadres" onfocus="empty(this, 'E-mailadres')" onblur="emptyOrDefault(this, 'E-mailadres')">
+        <?php ww_dash(); ?>
         <br />
-        <input type="password" name="wachtwoord" value="wachtwoord" onfocus="value='';" onblur="emptyOrDefault(this, 'wachtwoord')">
+        <input type="password" name="wachtwoord" value="wachtwoord" onfocus="empty(this, 'wachtwoord')" onblur="emptyOrDefault(this, 'wachtwoord')">
         <br />
         <input type="submit" value="Log in"> 
         <a href='wachtwoordvergeten.php' style='float: right;'><SMALL> Wachtwoord vergeten </SMALL></a>
         <br />
         <a href="registratie.html" style='float: right; margin-top: -4px; margin-bottom: 4px;'><SMALL>Registreren</SMALL></a>
     </form>
-    <?php
-} else {
-    echo $_SESSION['gebruiker-naam'];
-    ?>
-    <div id="ww-dash" class="clickable-item" onclick="window.location = 'winkelwagen.php'">
-        <?php    
-        $ww = Winkelwagen::try_load_from_session();
-        $aantal = '';
-        if (!$ww->is_empty())
-            $aantal = ' (' . count($ww->get_all()) . ')';
-        ?>
-        <div id="ww-content">
-            <?php echo $aantal; ?>
-        </div>
+<?php
+    } else {
+?>
+    <?php ww_dash(); ?>
+    <div id="account-opties">
+        <?php echo $_SESSION['gebruiker-naam']; ?><br />
+        <a href="account-overzicht.php">Mijn account</a><br />
+        <a href='inloggen.php'>Uitloggen</a>
     </div>
-    <br />
-    <a href="account-overzicht.php">Mijn account</a><br />
-    <a href='inloggen.php'>Uitloggen</a>
-    <?php
-}
+<?php
+    }
 ?>
