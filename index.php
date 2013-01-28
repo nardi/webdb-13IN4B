80c34c1045dcdeb753173c10c2ff8dfaa45a12f7
Nardi Lam <me@nardilam.nl>
2013-01-28 23:17:27 +0100

    Veranderingen voor validatie

--- index.php
+++ index.php
@@ -26,15 +26,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="Content of Site" content="Super Internet Shop, a webshop made by UvA" />
+        <meta name="Content of Site" content="Super Internet Shop, a webshop made by UvA students" />
         <link rel="shortcut icon" href="favicon.ico" />
         <title>SIS</title>
         <link rel="stylesheet" type="text/css" href="main.css" />
         <?php if (file_exists($pagename . ".css"))
                 echo '<link rel="stylesheet" type="text/css" href="' . $pagename . '.css" />';
         ?>    
-    <script type="text/javascript" src="button.js"></script>
-    
+        <script type="text/javascript" src="button.js"></script>  
 </head>
 
 <body onload = "setButtonColor(location.pathname)">
@@ -89,15 +88,14 @@
         </div>
     </div>
     <div id="footer" class="banner">
-		<a href="http://jigsaw.w3.org/css-validator/validator?uri=https://superinternetshop.nl/<?php echo $_SERVER['REQUEST_URI']; ?>">
+		<a href="http://jigsaw.w3.org/css-validator/validator?uri=https://superinternetshop.nl<?php echo $_SERVER['REQUEST_URI']; ?>">
 			<img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS3" />
 		</a>
-        <a href="http://validator.w3.org/check?uri=https://superinternetshop.nl/<?php echo $_SERVER['REQUEST_URI']; ?>">
+        <a href="http://validator.w3.org/check?uri=https://superinternetshop.nl<?php echo $_SERVER['REQUEST_URI']; ?>">
 			<img src="http://www.w3.org/Icons/valid-xhtml11-blue" alt="Valid XHTML 1.1" />
 		</a>
     </div>
 </div>
-
 
 <!-- source:http://snaptortoise.com/konami-js/ -->
 
@@ -106,7 +104,6 @@
 	konami = new Konami()
 	konami.load("?pag=42.toad");
 </script>
-
 
 </body>
 </html>
