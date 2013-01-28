2cf1ca00573c1b519f6249713cd9cbed1ec4657d
Nardi Lam <me@nardilam.nl>
2013-01-28 11:29:39 +0100

    Even de https-redirect weggehaald

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
      */
     if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS'] ) {
         $uri = 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'] ;
-        header('Location: '.$uri) ;
-}
+        //header('Location: '.$uri) ;
+    }
 ?>
 <?xml version="1.0"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
