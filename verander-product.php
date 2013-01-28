248fb7daba371dcd70ec07ac21bb4d9fcadcffc9
MetalPinguin <gitpinguin@gmail.com>
2013-01-28 13:10:49 +0100

    Foutmedlingen en such.

--- verander-product.php
+++ verander-product.php
@@ -44,6 +44,7 @@
             $sqli_verander->execute();
             
             if($cover!=NULL){
+                echo"NULL!";
                 $sqli_verander_cover = $db->prepare("UPDATE Producten SET cover=? WHERE id=?");
                 $sqli_verander_cover->bind_param('ss',$cover,$id);
                 $sqli_verander_cover->execute();
