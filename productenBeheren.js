40c98e02ca2affc6fe340b2d32c37f1829cff66b
MetalPinguin <gitpinguin@gmail.com>
2013-01-28 12:44:03 +0100

    Javascript aangepast.

--- productenBeheren.js
+++ productenBeheren.js
@@ -24,7 +24,8 @@ function enableEdit(id){
 }
 
 function uploadCover(coverId, id){
-    coverId.innerHTML="<input class='fileupload' type='file' name='image'"+id+">";
+    var image = 'image'+id;
+    coverId.innerHTML="<input class='fileupload' type='file' name='"+image+"'>";
     coverId.onclick="";
 }
 
