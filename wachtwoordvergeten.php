3d17024160ae21a3f3621cf0a5b350f908d75f5c
Stefani <kirakiraboshi82@gmail.com>
2013-01-28 11:48:06 +0100

    commentaar toegevoegd

--- wachtwoordvergeten.php
+++ wachtwoordvergeten.php
@@ -10,36 +10,53 @@
     
    
 <?php
-
+function show_form() 
+{
+	echo "<div align='justify'>
+		Als u uw wachtwoord bent vergeten, vul dan hieronder uw e-mailadres in. 
+		U ontvangt dan binnen enkele ogenblikken een e-mail van ons 
+		waarmee u een nieuw wachtwoord kunt opgeven. 
+		</div><br /><br />";
+	echo "<form method='post' action='wachtwoordvergeten.php'>
+		Email: <input name='email' type='text'><br />
+		</textarea><br />
+		<input type='submit' value='verstuur'>
+		</form>";
+}
 
 
 if (isset($_POST['email'])) {
+	
+	//naam en id worden opgehaald uit db, tegelijk wordt gekeken of email voorkomt in db
+	$email = $_POST['email'] ;
+	$db = connect_to_db();
+	$sql = $db->prepare("SELECT id, naam FROM Gebruikers WHERE email = ? LIMIT 1");
+	$sql->bind_param("s", $email) ;
+	$sql->bind_result($id, $naam) ;
+	$sql->execute();
+	$sql->fetch();
 
-	  $email = $_POST['email'] ;
-      $db = connect_to_db();
-      $sql = $db->prepare("SELECT naam FROM Gebruikers WHERE email = ? LIMIT 1");
-      $sql->bind_param("s", $email) ;
-	  $sql->bind_result($naam) ;
-      $sql->execute();
-
-      if (!$sql->fetch()) {
+	if (!$sql->fetch()) {
 	  
-        echo "Dit emailadres is niet bij ons geregistreerd." ;
+		echo "Dit emailadres is niet bij ons geregistreerd." ;
 		
-      } else {
+	} else {
 	    
+		//als emailadres bestaat in db, dan wordt er een token toegevoegd in de db 
 	    $sql->free_result();
 	    $token = md5($_POST['email'].time()) ;
 		$pwu = $db->prepare("UPDATE Gebruikers SET wachtwoord_token = '$token' WHERE email = ? LIMIT 1");
 		$pwu->bind_param("s", $email);
 		$pwu->execute();
 		
+		/*
 		$pwu->free_result();
 		$pwu2 = $db->prepare("SELECT id FROM Gebruikers WHERE wachtwoord_token = ? LIMIT 1");
 		$pwu2->bind_param("s", $token) ;
 		$pwu2->bind_result($id);
 		$pwu2->execute();
 		$pwu2->fetch();
+		*/
 		
         /*
 		$onderwerp = "Nieuw wachtwoord aanvragen" ;
@@ -73,28 +90,13 @@ if (isset($_POST['email'])) {
 		
      
         echo "U krijgt zo spoedig mogelijk een email toegestuurd met een link om uw wachtwoord opnieuw in te stellen.";
-      }
-    
+    }
+    //als er geen waarden zijn ingevuld, dan wordt het formulier weergegeven 
 } else {
-      
-    echo "<div align='justify'>
-    Als u uw wachtwoord bent vergeten, vul dan hieronder uw e-mailadres in. 
-    U ontvangt dan binnen enkele ogenblikken een e-mail van ons 
-    waarmee u een nieuw wachtwoord kunt opgeven. 
-    </div><br /><br />";
-	echo "<form method='post' action='wachtwoordvergeten.php'>
-      Email: <input name='email' type='text'><br />
-      </textarea><br />
-      <input type='submit' value='verstuur'>
-      </form>";
+	show_form();
+    
 }
-
-
-
 ?>
-     
-
-
       </div>
     </div>
 </div>
