a30c36453e7f135ec67db6ee49267f437a12fa48
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-28 12:39:46 +0100

    Opmaak van tabel aangepast

--- adplus-query.php
+++ adplus-query.php
@@ -3,9 +3,9 @@
 
     $emailquery ='%' . $_GET['email'] . '%';
   
-    $sqli = $db->prepare("SELECT naam, achternaam, email from Gebruikers WHERE email LIKE ? LIMIT 10");
+    $sqli = $db->prepare("SELECT naam, achternaam, email, status from Gebruikers WHERE email LIKE ? LIMIT 10");
     $sqli->bind_param('s', $emailquery);
-    $sqli-> bind_result($naam, $achternaam, $email);
+    $sqli-> bind_result($naam, $achternaam, $email, $status);
     echo $sqli->execute();
 
     echo "<table>";
@@ -13,7 +13,10 @@
     
     while ($sqli->fetch()) {
         echo "<tr>";
-        echo "<td>$naam</td> <td>$achternaam</td> <td>$email</td>";
+        echo "<td width=10pt>$naam</td> 
+            <td width=10pt>$achternaam</td> 
+            <td width=20pt>$email</td> 
+            <td width=5pt>$status</td>";
         echo "</tr>";
         
     }
