b744f60a7bad1e8ce95970458f13358b8305c5b3
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-28 14:52:22 +0100

    Bug in adplus-bewerken gefixed ?

--- adplus-bewerken.php
+++ adplus-bewerken.php
@@ -3,8 +3,8 @@
     
     $idedit = $_POST['id'];
     
-    $sqli = $db->prepare("SELECT naam, achternaam, telefoonnummer, email, status FROM Gebruikers WHERE id = ?");
-    $sqli->bind_param('i', $idedit);
+    $sqli = $db->prepare("SELECT naam, achternaam, telefoonnummer email, status from Gebruikers WHERE id=?);
+    $sqli->bind_param('s', $idedit);
     $sqli->bind_result($naam, $achternaam, $telefoonnummer, $email, $status);
     $sqli->execute();
     echo $naam . "...<br />";
