ee89f957ae6c2478a08e6300c1db1f3212de9642
MetalPinguin <gitpinguin@gmail.com>
2013-01-29 14:30:47 +0100

    Product verwijderen bijgfeschaafd.

--- product-verwijderen.php
+++ product-verwijderen.php
@@ -3,25 +3,12 @@
         function product_verwijderen_func($pid){
             $id=$pid;
             $db = connect_to_db();
-            $sqli_preserved_product = $db->prepare("SELECT * FROM Producten WHERE id=?");
-            $sqli_preserved_product->bind_param('i',$id);
-            $sqli_preserved_product->bind_result($useless,$titel,$platform_id,$genre_id,$beschrijving,$prijs,$release_date,$voorraad,$datum_toegevoegd,$cover);
-            $sqli_preserved_product->execute();
-            $sqli_preserved_product->fetch();
-            
-            $sqli_preserved_product->free_result();
-            
-            $sqli_preserved_product = $db->prepare("INSERT INTO Producten_Prullenbak VALUES (?,?,?,?,?,?,?,?,?,?)");
-            $sqli_preserved_product->bind_param('ssssssssss', $useless, $titel, $platform_id, $genre_id, $beschrijving, $prijs, $release_date, $voorraad, $datum_toegevoegd, $cover);
-            
-            $sqli_preserved_product->execute();
-            
-            $sqli_destroy_product = $db->prepare("DELETE FROM Producten WHERE id=?");
-            $sqli_destroy_product->bind_param('i',$id);
-            $sqli_destroy_product->execute();
+            $sqli_verwijderen = db->prepare("UPDATE Producten SET verwijderd=1 WHERE id=?");
+            $sqli_verwijderen->bind_param('i',$id);
+            $sqli_verwijderen->execute();
             
         
-            echo "<img src='placeholder' /> <br /> <strong> $titel is succesvol verwijderd uit de database.</strong>";
+            echo "<img src='images/labels/verwijderd.gif' /> <br /> <strong> $titel is succesvol verwijderd uit de database.</strong>";
         }
         if(isset($_POST['delete'])){
             $iid=$_POST['delete'];
