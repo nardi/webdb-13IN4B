ce34653aa7b974926e8bdbf77265f6cd2fb61c7a
MetalPinguin <gitpinguin@gmail.com>
2013-01-29 15:34:31 +0100

    Dingen gedaan.

--- sidebar.php
+++ sidebar.php
@@ -21,7 +21,7 @@
 <form method="get" action="products.php" id='searchform'>
     <div>
         <input type="text" name="search"/>
-        <div id='searchbutton' onclick='document.getElementById('searchform').submit()'>
+        <div id='searchbutton' onclick="document.getElementById('searchform').submit()">
         </div>
     </div>
 </form>
