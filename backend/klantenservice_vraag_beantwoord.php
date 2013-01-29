71cea0f4a1a84674db9d67d75cf1a558e4765ddf
Stefani <kirakiraboshi82@gmail.com>
2013-01-29 20:58:46 +0100

    kleine aanpassing

--- backend/klantenservice_vraag_beantwoord.php
+++ backend/klantenservice_vraag_beantwoord.php
@@ -7,3 +7,6 @@ echo "Het doet ons een genoegen dat wij uw vraag hebben kunnen beantwoorden.";
 <script type="text/JavaScript">
                 setTimeout("location.href = '/';",2500);
 </script>
+
+<?php
+?>
