50375ac4bd566553c84a8539b1db1cf34c7e00a7
Nardi Lam <me@nardilam.nl>
2013-01-29 21:41:53 +0100

    Voorraadcontrole werkt nu volledig volgens mij :)

--- winkelwagen.js
+++ winkelwagen.js
@@ -2,24 +2,21 @@ function changeAmount(id)
 {
     var xhr = new XMLHttpRequest();
     var amountElem = document.getElementById('amount-' + id);
-    var amount = parseInt(amountElem.value);
-    if (!isNaN(amount))
+    var amount = amountElem.value;
+    var url = 'backend/hoeveelheden.php';
+    var params = 'amount-' + id + '=' + amount;
+    xhr.open('POST', url);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Content-length", params.length);
+    xhr.setRequestHeader("Connection", "close");
+    xhr.onreadystatechange = function()
     {
-        var url = 'backend/hoeveelheden.php';
-        var params = 'amount-' + id + '=' + amount;
-        xhr.open('POST', url);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.setRequestHeader("Content-length", params.length);
-        xhr.setRequestHeader("Connection", "close");
-        xhr.onreadystatechange = function()
+        if (xhr.readyState == 4)
         {
-            if (xhr.readyState == 4)
-            {
-                recalculateTotalPrice(JSON.parse(xhr.responseText));
-            }
-        };
-        xhr.send(params);
-    }
+            recalculateTotalPrice(JSON.parse(xhr.responseText));
+        }
+    };
+    xhr.send(params);
 }
 
 function recalculateTotalPrice(ww)
@@ -31,26 +28,16 @@ function recalculateTotalPrice(ww)
         document.getElementById('amount-' + id).value = amount;
         var price = parseFloat(document.getElementById('price-' + id).innerHTML.replace(',', '.'));
         var productPrice = Math.round(amount * price * 100) / 100;
-        document.getElementById('productprice-' + id).innerHTML = productPrice.toFixed(2).replace('.', ',');
+        document.getElementById('productprice-' + id).innerHTML = formatPrice(productPrice);
         totalPrice += productPrice;
     }
     totalPrice = Math.round(totalPrice * 100) / 100;
-    document.getElementById('total-price').innerHTML = totalPrice.toFixed(2).replace('.', ',');
+    document.getElementById('total-price').innerHTML = formatPrice(totalPrice);
 }
 
-/* function recalculateTotalPrice()
+function formatPrice(prijs)
 {
-    var ids = document.getElementsByName('product-id');
-    var totalPrice = parseFloat(document.getElementById('shipping').innerHTML.replace(',', '.'));
-    for (var i = 0; i < ids.length; i++)
-    {
-        var id = ids[i].innerHTML;
-        var amount = parseInt(document.getElementById('amount-' + id).value);
-        var price = parseFloat(document.getElementById('price-' + id).innerHTML.replace(',', '.'));
-        var productPrice = Math.round(amount * price * 100) / 100;
-        document.getElementById('productprice-' + id).innerHTML = productPrice.toFixed(2).replace('.', ',');
-        totalPrice += productPrice;
-    }
-    totalPrice = Math.round(totalPrice * 100) / 100;
-    document.getElementById('total-price').innerHTML = totalPrice.toFixed(2).replace('.', ',');
-} */
+    if (Math.round(prijs) == prijs)
+        return prijs.toFixed() + ',-';
+    return prijs.toFixed(2).replace('.', ',');
+}
