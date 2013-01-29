148ff0727622a221d0e262d9a64eb411b80bfe79
Stefani <kirakiraboshi82@gmail.com>
2013-01-29 23:21:49 +0100

    code aangepast

--- backend/klantenservice_factuur_niet_ontvangen.php
+++ backend/klantenservice_factuur_niet_ontvangen.php
@@ -1,52 +1,43 @@
 <link rel="stylesheet" type="text/css" href="inloggen-wachtwoord-registratie.css">
 
-
 <script src="klantenservice.js">
 </script>
-</head>
-
 
 <?php
 	session_start();
-	if ((!isset($_SESSION['logged-in']))) {
+if ((!isset($_SESSION['logged-in']))) {
 	?>
 	<pre>
-U bent niet ingelogd!
+	U bent niet ingelogd!
 	</pre>
 	<?php
-	} else {
+} else {
 	?>
 	<div class="centered-container"> 
-		<div class="account-wachtwoord-veranderen">
-		
-		<hr width="100%">
-		
-		<p> U heeft aangegeven dat uw geen factuur heeft ontvangen.
-		Wij sturen u altijd een orderbevestiging waarin de factuur weergegeven wordt. 
-		Het kan zijn dat de orderbevestiging in uw spamfolder terecht is gekomen.
-		Controleer alstublieft even uw spamfolder.
-		Tevens sturen wij nogmaals dezelfde factuur met de bestelling mee. </p>
-		<br /><br />
-		
-		<form action="../">
-		Is hiermee uw vraag beantwoord?<br />
-		<select onchange="window.open(this.options[this.selectedIndex].value,'_top')">
-		
-		<option value="">kies een antwoord...</option>
-		<option value="klantenservice_vraag_beantwoord.php">ja</option>
-		<option value="klantenservice_niet_beantwoord.php">nee</option>  
-		</select>
-		</form>
-		
-		
-		
-		
-		
-		</div>
+	<div class="account-wachtwoord-veranderen">
+	<hr width="100%">
+
+	<p> U heeft aangegeven dat uw geen factuur heeft ontvangen.
+	Wij sturen u altijd een orderbevestiging waarin de factuur weergegeven wordt. 
+	Het kan zijn dat de orderbevestiging in uw spamfolder terecht is gekomen.
+	Controleer alstublieft even uw spamfolder.
+	Tevens sturen wij nogmaals dezelfde factuur met de bestelling mee. </p>
+	<br /><br />
+
+	<form action="../">
+	Is hiermee uw vraag beantwoord?<br />
+	<select onchange="window.open(this.options[this.selectedIndex].value,'_top')">
+
+	<option value="">kies een antwoord...</option>
+	<option value="klantenservice_vraag_beantwoord.php">ja</option>
+	<option value="klantenservice_niet_beantwoord.php">nee</option>  
+	</select>
+	</form>
+
 	</div>
-		</body>
-	<?php
-	}
-	?>
-	
-	
+	</div>
+
+<?php
+}
+?>
+
