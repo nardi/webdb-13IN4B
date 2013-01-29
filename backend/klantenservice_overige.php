c6af6b30fc087aec7406da87bbf736cca83f9c83
Stefani <kirakiraboshi82@gmail.com>
2013-01-29 23:31:47 +0100

    artikel voorraad toegevoegd

--- backend/klantenservice_overige.php
+++ backend/klantenservice_overige.php
@@ -21,13 +21,11 @@ if ((!isset($_SESSION['logged-in']))) {
 	echo "U heeft aangegeven dat uw vraag er niet tussen staat.
 		Klikop onderstaande link om ons een bericht te sturen.";
 	?>
-	<br /><br >
+	<br /><br />
 	<a href="/klantenservice_niet_beantwoord.php">ga naar formulier</a>
-	<script type="text/JavaScript">
-	   setTimeout("location.href = '/klantenservice_niet_beantwoord.php';",3500);
-	</script>
 	
 	</div>
 	</div>
-	<?php
+<?php
 }
+?>
