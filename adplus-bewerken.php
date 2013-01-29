1642b8d65a35991b15b9b0433c036d2352b4e259
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-29 11:22:16 +0100

    Bug in adplus gefixed

--- adplus-bewerken.php
+++ adplus-bewerken.php
@@ -26,11 +26,12 @@
         $sqli = $db->prepare("SELECT naam, achternaam, telefoonnummer, email, status FROM Gebruikers WHERE id=?");
         $sqli->bind_param('i', $idedit);
         $sqli->bind_result($naam, $achternaam, $telefoonnummer, $email, $status);
-        echo $sqli->execute();
-        echo $naam . "...<br />";
-        
+        $sqli->execute();
+                
         echo '<form method="post" action="' .$_SERVER['PHP_SELF']. '">';
-       
+        
+        echo "<input type='submit' name='submit' value='Wijzigingen opslaan'>";
+        
         echo "<h1> Gebruikers gegevens </h1>";
         echo "<table>
             <tr>
