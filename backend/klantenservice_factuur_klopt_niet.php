af80af1a4fdfbac5f3054cf192a40eb5c1e4c05b
Stefani <kirakiraboshi82@gmail.com>
2013-01-29 21:04:27 +0100

    laatste choice aangepast

--- backend/klantenservice_factuur_klopt_niet.php
+++ backend/klantenservice_factuur_klopt_niet.php
@@ -24,19 +24,21 @@ U bent niet ingelogd!
 		<p> Het kan voorkomen dat er iets niet klopt op de factuur. 
 		Wij doen altijd onze best om de prijzen up-to-date te houden. 
 		Houd er wel rekening mee dat wij standaard altijd €6,75 verzendkosten in rekening brengen. </p>
-		<form>
+		
+		<form action="../">
 		Is hiermee uw vraag beantwoord?<br />
-		<select id="myList" onchange="loadXMLDoc(this.options[this.selectedIndex].value,'choice_factuurprobleem')">
+		<select onchange="window.open(this.options[this.selectedIndex].value,'_top')">
+		
 		<option value="">kies een antwoord...</option>
 		<option value="backend/klantenservice_vraag_beantwoord.php">ja</option>
 		<option value="backend/klantenservice_niet_beantwoord.php">nee</option>  
 		</select>
 		</form>
 		
-		<div id="choice_factuurprobleem">
 		
 		
-		</div>
+		
+		
 		</div>
 	</div>
 		</body>
--- backend/klantenservice_vraag_beantwoord.php
+++ backend/klantenservice_vraag_beantwoord.php
@@ -5,7 +5,7 @@ echo "Het doet ons een genoegen dat wij uw vraag hebben kunnen beantwoorden.";
 ?>
 
 <script type="text/JavaScript">
-                setTimeout("location.href = '/';",2500);
+                setTimeout("location.href = '/index.php';",2500);
 </script>
 
 <?php
