f398e8a22fdba73f259f0a57acb705e0ab9f26e2
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-30 11:25:08 +0100

    Kleine dingetjes toegevoegd

--- aanbieding-toevoegen.php
+++ aanbieding-toevoegen.php
@@ -15,8 +15,8 @@
                     Product: 
                     <select name="producten">
                     <?php
-                        $productensql = $db->prepare("SELECT id, titel FROM Producten");
-                        $productensql->bind_result($productid, $product);
+                        $productensql = $db->prepare("SELECT id, titel, prijs FROM Producten");
+                        $productensql->bind_result($productid, $product, $prijs);
                         $productensql->execute();
                         
                         while ($productensql->fetch()) {
@@ -28,12 +28,12 @@
                         $db->close();
                     ?>
                     </select> <br />
+                    Huidige prijs: <input type="text" disabled="disabled" name="prijs" value="<?php echo "$prijs"; ?>"/><br />
+                    Aanbiedingsprijs: <input type="text" name="prijs" /><br />
+                    Begin-datum: <input type="text" name="begin-datum" /><br />
+                    Eind-datum: <input type="text" name="eind-datum" /><br />
                     
-                    Aanbiedingsprijs: <input type="text" name="prijs"><br />
-                    Begin-datum: <input type="text" name="begin-datum"><br />
-                    Eind-datum: <input type="text" name="eind-datum"><br />
-                    
-                    <input type="submit" value="Voeg aanbieding toe">
+                    <input type="submit" value="Voeg aanbieding toe" />
            
                     
                     
