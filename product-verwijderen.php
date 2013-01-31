50c128b2138b8da9b7d74b9c6a363ecae8bc75ab
MetalPinguin <gitpinguin@gmail.com>
2013-01-31 12:39:18 +0100

    Titel wordt weergegeven bij verwijder message.

--- product-verwijderen.php
+++ product-verwijderen.php
@@ -1,13 +1,14 @@
 <?php
     if(is_admin()){
         function product_verwijderen_func($pid, $titel){
+            $thistitel =$titel;
             $id=$pid;
             $db = connect_to_db();
             $sqli_verwijderen = $db->prepare("UPDATE Producten SET verwijderd=1 WHERE id=?");
             $sqli_verwijderen->bind_param('i',$id);
             $sqli_verwijderen->execute();
         
-            echo "<img src='images/labels/verwijderd.gif' /> <br /> <strong> $titel is succesvol verwijderd uit de database.</strong>";
+            echo "<img src='images/labels/verwijderd.gif' /> <br /> <strong> $thistitel is succesvol verwijderd uit de database.</strong>";
         }
         if(isset($_POST['delete'])){
             $iid=$_POST['delete'];
--- verander-product.php
+++ verander-product.php
@@ -5,16 +5,9 @@
     $sqli_id = $db->prepare("SELECT id FROM Producten");
     $sqli_id->bind_result($id);
     $sqli_id->execute();
-    //Dit bestand moet worden teruggezet naar een andere GIT versie.
     $sqli_id->store_result();
-   // $sqli_id->free_result();*/
-    ?><script>//alert("Main");</script><?php
-    var_dump($_POST);
     while($sqli_id->fetch()){
-        //echo "ID=$id <br>";
-        ?><script>//alert("while");</script><?php
         if(isset($_POST["titel$id"]) && $_POST["verwijderen"] != 'teverwijderen'){
-            ?><script>//alert("if");</script><?php
             $titel=$_POST["titel$id"];
             $platform_id=$_POST["platform$id"];
             $genre_id=$_POST["genre$id"];
@@ -22,10 +15,8 @@
             $prijs=$_POST["prijs$id"];
             $release_date=$_POST["release$id"];
             $voorraad=$_POST["voorraad$id"];
-            //$id=$_POST['idee'];
             
            try{
-                echo "TRY";
                 $image = upload_image("file$id");
             }
             catch(Exception $img){
@@ -42,24 +33,17 @@
             $sqli_verander->execute();
             
             if($image!=NULL){
-                echo"Niet NULL!";
                 $sqli_verander_cover = $db->prepare("UPDATE Producten SET cover=? WHERE id=?");
                 $sqli_verander_cover->bind_param('ss',$image,$id);
                 $sqli_verander_cover->execute();
                 
             }
-            
-            else if($image==NULL){
-                echo "HOERA NULL! $id";
-            }
         } 
-
+        
         else if($_POST["verwijderen$id"] == 'teverwijderen'){
+            $titel=$_POST["titel$id"];
             product_verwijderen_func($id, $titel);
-        }
-        //$titeltest=$_POST['titel22'];    
-        //echo "$titeltest";
-
+        }        
     }
     $db->close();
 ?>
