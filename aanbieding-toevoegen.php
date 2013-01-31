9497496816bc7eaf58127a1cab0de6e42d101572
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-31 15:48:15 +0100

    Errors fixen in aanbiedingen

--- aanbieding-toevoegen.php
+++ aanbieding-toevoegen.php
@@ -8,10 +8,10 @@
         <div class="centered-container">
             <form action="aanbieding-toevoegen-db.php" method="post">
                 <h1><b>Aanbieding toevoegen</b></h1>
-                <hr width="100%">
+                <hr />
                 <b>Aanbiedings-gegevens</b>
                 <br />
-                <div class = "right_align">
+                <p class = "right_align">
                     Product: 
                     <select name="producten">
                     <?php
@@ -39,7 +39,7 @@
                     ?>
                     
                     
-                </div>
+                </p>
             </form>
         </div>
     <?php
