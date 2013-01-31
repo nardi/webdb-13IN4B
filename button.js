5e248640cce715a35dd9b96e4b2c7d402c0189b6
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-31 14:11:19 +0100

    Opmaak aan aanbiedingen, en error in button.js gefixed

--- button.js
+++ button.js
@@ -6,7 +6,9 @@ function onButtonclick(button) {
 
 
 function setButtonColor(button) {
-    document.getElementById(button).style.background = '#08C1FF';
-    document.getElementById(button).style.color = '#FFFFFF';
-    document.getElementById(button).style.fontWeight="bold";   
+    if(button) {
+        document.getElementById(button).style.background = '#08C1FF';
+        document.getElementById(button).style.color = '#FFFFFF';
+        document.getElementById(button).style.fontWeight="bold";   
+    }
 }
