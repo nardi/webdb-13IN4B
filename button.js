3b84f5b193eab4975c35b13c7d784b4971ed6378
Jordy Perlee <jordy.perlee@gmail.com>
2013-01-31 14:53:19 +0100

    Commentaar

--- button.js
+++ button.js
@@ -1,10 +1,15 @@
+/* onButtonClick laad een pagina aan de hand op welke knop in de sidebar wordt
+ * gedrukt.
+ */
 function onButtonclick(button) {
     if (!button)
         button = '';
     window.location = '/' + button;
 }
 
-
+/* De setButtonColor functie kijkt of de beklikte div een knop in de sidebar is.
+ * Zo ja, dan wordt deze knop ge-highlight.
+ */
 function setButtonColor(button) {
     var buttonObject = document.getElementById(button);
     if(buttonObject != null) {
