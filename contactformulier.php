0ae01b06200ef3c5c21a38fd43659f292c088084
Stefani <kirakiraboshi82@gmail.com>
2013-01-31 12:37:32 +0100

    kleine aanpassing

--- contactformulier.php
+++ contactformulier.php
@@ -16,7 +16,7 @@ de klant input te vragen, en deze door te sturen naar ons emailadres.
 function show_form()
 {
 echo "<form method='post' action='contactformulier.php'>
-	Email: <br /><input name='email' type='text'><br />
+	Voer hier uw emailadres in: <br /><input name='email' type='text'><br />
 	Onderwerp: <br /><input name='onderwerp' type='text'><br />
 	Bericht: <br />
 	<textarea name='bericht' rows='15' cols='40'></textarea><br />
