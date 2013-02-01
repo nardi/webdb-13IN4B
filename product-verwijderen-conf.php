e071e26b13db31b7a14b0cab8f0b58e13cb235f7
MetalPinguin <MetalPinguinInc@gmail.com>
2013-02-01 01:54:37 +0100

    Typo.

--- product-verwijderen-conf.php
+++ product-verwijderen-conf.php
@@ -9,14 +9,14 @@
                 <div id='ja'>
                     <form id='DeleteItemConf' action='products.php' method='post'>
                         <div id='ItemVerwijderen'>
-                            <input type='submit' value='Ja' name='submitButtonYes' id='DeleteSubmitButton' title='Product verwijderen'/>
+                            <input type='submit' value='Ja' name='submitButtonYes' id='DeleteSubmitButtonYes' title='Product verwijderen'/>
                         </div>
                     </form>
                 </div>
                 <div id='nee'>
                     <form id='backToProduct' action='redirect_to(item-description.php?id=$pid)' method='post'>
                         <div id='ItemNietVerwijderen'>
-                            <input type='submit' value='Nee' name='submitButtonNo' id='DeleteSubmitButton' title='Product verwijderen'/>
+                            <input type='submit' value='Nee' name='submitButtonNo' id='DeleteSubmitButtonNo' title='Product verwijderen'/>
                         </div>
                     </form>
                 </div>
